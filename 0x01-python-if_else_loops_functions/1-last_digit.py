b00cd8ff27fbaa943b2021da66a470736999178a
Karoline-S <karoline.amy@gmai.com>
2022-04-26 14:17:17 -0700

    build - add task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    last_digit = int(repr(number)[0])
+else
+    print("Last digit of", number, "is", last_digit, "and is greater than 
