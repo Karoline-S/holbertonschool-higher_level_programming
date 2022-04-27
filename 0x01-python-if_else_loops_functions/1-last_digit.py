c1d494f9bb5d7ee5271a8c11460263cde59bbaee
Karoline-S <karoline.amy@gmai.com>
2022-04-26 20:03:54 -0700

    fix - output type and remove cast to int

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 number_str = str(number)
-last_digit = int(str(number_str[-1]))
+last_digit = str(number_str[-1])
 
 if number > 0 and last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is zero")
+    print(f"Last digit of {number} is {last_digit} and is 0")
 elif number > 0 and last_digit < 6:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and"
-          " not zero")
+          " not 0")
 else:
     print(f"Last digit of {number} is -{last_digit} and is less than"
-          " 6 and not zero")
+          " 6 and not 0")
