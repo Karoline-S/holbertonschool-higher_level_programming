4e874991ff35d51ead1187de1c13b09d1c0410bb
Karoline-S <karoline.amy@gmai.com>
2022-04-26 21:21:18 -0700

    build - add task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if char >= 'a' and char <= 'z':
+            char = ord(char) - 32
+            char = chr(char)
+        print(char, end="")
+    print()
