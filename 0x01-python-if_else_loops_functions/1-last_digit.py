0d06e4ff45e5f97adb409ace2794caacef8bb4c2
Karoline-S <karoline.amy@gmai.com>
2022-04-26 19:59:47 -0700

    fix - change logic to account for negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+number_str = str(number)
+last_digit = int(str(number_str[-1]))
 
-if number >= 0:
-    last_digit = number % 10
-    if last_digit > 5:
-        print(f"Last digit of {number} is {last_digit} and is greater than 5")
-    elif last_digit == 0:
-        print(f"Last digit of {number} is {last_digit} and is zero")
-    else:
-        print(f"Last digit of {number} is {last_digit} and is less than 6 and not zero")
-
+if number > 0 and last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+elif last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is zero")
+elif number > 0 and last_digit < 6:
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and"
+          " not zero")
 else:
-    last_digit = ((number * -1) % 10) * -1
-    if last_digit > 5:
-        print(f"Last digit of {number} is {last_digit} and is greater than 5")
-    elif last_digit == 0:
-        print(f"Last digit of {number} is {last_digit} and is zero")
-    else:
-        print(f"Last digit of {number} is {last_digit} and is less than 6 and not zero")
+    print(f"Last digit of {number} is -{last_digit} and is less than"
+          " 6 and not zero")
