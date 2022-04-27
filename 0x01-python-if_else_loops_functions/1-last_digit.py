2a2c2fb1db0d432cfc5b141f3f419de51388841c
Karoline-S <karoline.amy@gmai.com>
2022-04-26 20:06:23 -0700

    fix - move cast into if and elif statements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,11 @@ number = random.randint(-10000, 10000)
 number_str = str(number)
 last_digit = str(number_str[-1])
 
-if number > 0 and last_digit > 5:
+if number > 0 and int(last_digit) > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
+elif int(last_digit) == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
-elif number > 0 and last_digit < 6:
+elif number > 0 and int(last_digit) < 6:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and"
           " not 0")
 else:
