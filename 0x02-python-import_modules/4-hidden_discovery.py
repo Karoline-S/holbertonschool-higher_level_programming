241f3e61db26dee5b6fa13b499886ea4e487eca7
Karoline-S <karoline.amy@gmai.com>
2022-04-27 21:32:27 -0700

    build - add task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import hidden_4
+
+    list_names = dir(hidden_4)
+
+    for name in list_names:
+        print(name.replace("__", ""))
