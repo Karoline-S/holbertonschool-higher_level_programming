9235b59d2774d8cc1d18354eb012f80fa4d31089
Karoline-S <karoline.amy@gmai.com>
2022-05-02 06:08:34 -0700

    fix - account for mirror image requirement, not just tally

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -19,6 +19,29 @@ int list_length(listint_t *head)
 }
 
 /**
+ * compare_nodes - compares the int held in two different nodes
+ * @head: a pointer to the start of the list
+ * @length: the length of the list
+ * Return: the difference between the two ints
+ */
+int compare_nodes(listint_t *head, int length)
+{
+	int node1, node2;
+
+	node1 = head->n;
+
+	while (length > 1)
+	{
+		head = head->next;
+		length--;
+	}
+
+	node2 = head->n;
+
+	return (node1 - node2);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: a double pointer to the start of the list
  *
@@ -26,7 +49,7 @@ int list_length(listint_t *head)
  */
 int is_palindrome(listint_t **head)
 {
-	int tally = 0, halfway = 0, length = 0;
+	int check, halfway = 0, length = 0;
 
 	if (head == NULL || *head == NULL)
 		return (1);
@@ -40,22 +63,13 @@ int is_palindrome(listint_t **head)
 
 	while (halfway > 0)
 	{
-		tally += (*head)->n;
+		check = compare_nodes(*head, length);
+		if (check != 0)
+			return (0);
 		*head = (*head)->next;
 		halfway--;
+		length--;
 	}
 
-	if (length % 2 != 0)
-		*head = (*head)->next;
-
-	while (*head != NULL)
-	{
-		tally -= (*head)->n;
-		*head = (*head)->next;
-	}
-
-	if (tally == 0)
-		return (1);
-	else
-		return (0);
+	return (1);
 }
