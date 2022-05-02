12e192145e3c8f5ae37b883ded54c83eb1851762
Karoline-S <karoline.amy@gmai.com>
2022-05-01 23:10:00 -0700

    build -  add task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    else:
+        biggest = my_list[0]
+        for num in my_list:
+            if num > biggest:
+                biggest = num
+    return biggest
