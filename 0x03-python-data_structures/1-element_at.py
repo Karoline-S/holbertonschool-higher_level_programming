b85c15e07a2823be072b3bc9730ad7f67f3e2e18
Karoline-S <karoline.amy@gmai.com>
2022-05-01 21:46:48 -0700

    test - add brackets to return statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx >= len(my_list) - 1:
+    if idx >= len(my_list) - 1 or len(my_list) == 0:
         return None
 
     if idx < 0:
         return None
 
-    return my_list[idx]
+    return(my_list[idx])
