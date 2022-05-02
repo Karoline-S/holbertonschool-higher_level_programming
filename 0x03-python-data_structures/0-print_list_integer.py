69a3b117ee0780cb9ee366971f6dbe0792214a85
Karoline-S <karoline.amy@gmai.com>
2022-05-01 21:29:44 -0700

    fix - print only integers in a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        return
-
     for item in my_list:
-        print(item)
+        if isinstance(item, int):
+            print(item)
