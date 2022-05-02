1e617ae8283f44a37155286fe199dbf070e2d80e
Karoline-S <karoline.amy@gmai.com>
2022-05-01 23:21:07 -0700

    fix - remove unnecessary else

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,8 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
-    else:
-        biggest = my_list[0]
-        for num in my_list:
-            if num > biggest:
-                biggest = int(num)
+    biggest = my_list[0]
+    for num in my_list:
+        if num > biggest:
+            biggest = num
     return biggest
