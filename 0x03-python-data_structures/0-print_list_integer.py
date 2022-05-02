c28d786c8948c6f77e3f3a30666f81432a250caf
Karoline-S <karoline.amy@gmai.com>
2022-05-01 21:32:25 -0700

    fix - remove shebang

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for item in my_list:
-        if isinstance(item, int):
+
+    if len(my_list) > 0:
+        for item in my_list:
             print(item)
