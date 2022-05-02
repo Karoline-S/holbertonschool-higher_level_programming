1a804d862d99390419f8283a889ac39046dfe4e7
Karoline-S <karoline.amy@gmai.com>
2022-05-01 22:03:53 -0700

    fix - remove -1 from list len comparison

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list.copy()
-
-    if idx >= len(my_list) - 1:
+    if idx < 0 idx >= len(my_list):
         return my_list.copy()
 
     new_list = list(my_list)
