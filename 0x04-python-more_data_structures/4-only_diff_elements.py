de9fb53f92f7630658462a3050025408fe2bbbb3
Karoline-S <karoline.amy@gmai.com>
2022-05-03 21:34:23 -0700

    fix - pycodestyle variable name issue

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    l = [set_1, set_2]
+    a_list = [set_1, set_2]
     new_set = set()
-    for item in l:
-        new_set |= l[0] - set.union(*l[1:])
-        l = l[-1:] + l[:-1]
+    for item in a_list:
+        new_set |= a_list[0] - set.union(*a_list[1:])
+        a_list = a_list[-1:] + a_list[:-1]
     return new_set
