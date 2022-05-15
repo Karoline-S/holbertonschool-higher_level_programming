2171630ac8eafb1eb3283f0cd32cc585e56f70f6
Karoline-S <karoline.amy@gmai.com>
2022-05-15 16:59:47 -0700

    build - add task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    count = 0
+    new_list = []
+
+    for i in range(x):
+        try:
+            new_list.append(my_list[i])
+            count += 1
+        except IndexError:
+            break
+    print(*new_list, sep="")
+
+    return count
