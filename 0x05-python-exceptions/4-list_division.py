0039082c74f801b8219517841a2f93713f19217f
Karoline-S <karoline.amy@gmai.com>
2022-05-15 20:31:49 -0700

    fix - add 0 to new list when index out of range

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,6 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
             new_list.append(0)
             print("division by 0")
         except IndexError:
+            new_list.append(0)
             print("out of range")
             break
     return new_list
