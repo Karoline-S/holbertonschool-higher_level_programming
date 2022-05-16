38ca070a0de842bd68febccc39f6ccd745e47d24
Karoline-S <karoline.amy@gmai.com>
2022-05-15 20:04:21 -0700

    fix - adjust zero divison exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,7 +7,8 @@ def safe_print_division(a, b):
         result = a / b
 
     except ZeroDivisionError:
-        return
+        result = None
+        return None
 
     finally:
             print("Inside result: {}".format(result))
