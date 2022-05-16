afbf362ce3723d9bc94c5bdde054ad2a731cce0f
Karoline-S <karoline.amy@gmai.com>
2022-05-15 19:36:57 -0700

    fix - remove if and add type and value error check

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,12 +4,13 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
 
     for i in range(x):
-        if type(i) == int:
-            try:
-                print("{:d}".format(my_list[i]), sep="", end="")
-                count += 1
-            except IndexError:
-                break
+        try:
+            print("{:d}".format(my_list[i]), sep="", end="")
+            count += 1
+        except (TypeError, ValueError):
+            continue
+        except IndexError:
+            break
 
     print()
     return count
