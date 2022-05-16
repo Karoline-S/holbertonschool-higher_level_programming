929266689552a7fd6874f77dc77e439a856a6119
Karoline-S <karoline.amy@gmai.com>
2022-05-15 19:25:57 -0700

    fix - add valueerror

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for i in range(x):
         try:
-            if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), sep="", end="")
-                count += 1
+            print("{:d}".format(my_list[i]), sep="", end="")
+            count += 1
         except (IndexError, ValueError):
             break
 
