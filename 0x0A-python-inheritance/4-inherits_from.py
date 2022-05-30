fc55d771a6cdb0385fb862a52b8bfe995d9b4c60
Karoline-S <karoline.amy@gmai.com>
2022-05-29 23:39:31 -0700

    docs - add docstring

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
+    """a function to check whether obj inherited from a_class's super classes
+    """
+
     if type(obj) == a_class:
         return False
     return issubclass(type(obj), a_class)
