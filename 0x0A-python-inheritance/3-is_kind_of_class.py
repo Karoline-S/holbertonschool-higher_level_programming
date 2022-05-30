ff20d632e90eb6b03a1fa04f87721fbe5940bf7f
Karoline-S <karoline.amy@gmai.com>
2022-05-29 23:36:42 -0700

    docs - add docstring

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def is_kind_of_class(obj, a_class):
-    return type(obj) is a_class
+    """a function to check whether obj is an instance of a_class
+    includes inherited classes
+    """
+
+    return isinstance(obj, a_class)
