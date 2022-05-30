9df51633b1ddb2c8824889a100c301357888be90
Karoline-S <karoline.amy@gmai.com>
2022-05-30 01:48:07 -0700

    fix - make docstrings longer to try to meet arbitrary requirements

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
-"""a module containing a single function:
+"""
+a module containing a single function:
 is_same_class()
 """
 
 
 def is_same_class(obj, a_class):
-    """a function to check whether an object is of a particular class
+    """
+    a function to check whether an object is of a particular class
     """
 
     return type(obj) == a_class
