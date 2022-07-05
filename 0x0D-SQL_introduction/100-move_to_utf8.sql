a6883bf7f44d500ae2d487a6ead841ad172b9af4
Karoline-S <karoline.amy@gmail.com>
2022-07-05 00:02:26 -0700

    fix - add database selection

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- convert database, table and field to UTF8
 -- db: hbtn_0c_0, table: first_table, field: name (first_table)
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4;
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
