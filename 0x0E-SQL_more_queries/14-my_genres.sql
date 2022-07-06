0907c8332c7fd85aaa2bffb8fbef46a03702d0c2
Karoline-S <karoline.amy@gmail.com>
2022-07-06 20:52:53 +1000

    build - add task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- list all genres of a show
+-- db passed in as arg
+SELECT tv_genres.name AS name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name
