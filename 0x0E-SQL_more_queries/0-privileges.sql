15e712be59e84ce7fbd156c124fcdd8223f2607c
Karoline-S <karoline.amy@gmail.com>
2022-07-06 10:44:57 +1000

    "build - add task 0"

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- lists all privileges of two specific users on server (in localhost)
+-- users in this task are user_0d_1 and user_0d_2
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
