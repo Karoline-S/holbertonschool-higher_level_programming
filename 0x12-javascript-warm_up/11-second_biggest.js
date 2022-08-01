be070fa6b947db0d82290e09fa92426ce3d7f5ce
Karoline-S <karoline.amy@gmail.com>
2022-08-01 18:43:41 +1000

    fix - remove loops

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,11 @@
 #!/usr/bin/node
 
-if (process.argv.length < 4) {
+const array = process.argv;
+
+if (array.length < 4) {
   console.log('0');
 } else {
-  let i = 2;
-  const numArray = [];
-
-  while (process.argv[i]) {
-    numArray.push(Number(process.argv[i]));
-    i++;
-  }
-
-  let biggest = numArray[0];
-  let nextBiggest = numArray[0];
-
-  for (i = 0; i < numArray.length; i++) {
-    if (numArray[i] > biggest) {
-      nextBiggest = biggest;
-      biggest = numArray[i];
-    } else if (numArray[i] > nextBiggest) {
-      nextBiggest = numArray[i];
-    }
-  }
-  console.log(nextBiggest);
+  array.sort();
+  const twoBiggest = array.splice(array.length - 2);
+  console.log(twoBiggest[0]);
 }
