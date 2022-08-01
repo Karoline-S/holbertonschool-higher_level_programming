80affd0b1824ecffde884ca3b9c4e33caf0fb23d
Karoline-S <karoline.amy@gmail.com>
2022-08-01 13:08:36 +1000

    fix - account for multiples

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -18,7 +18,7 @@ if (process.argv.length < 4) {
     if (numArray[i] > biggest) {
       nextBiggest = biggest;
       biggest = numArray[i];
-    } else if (numArray[i] > nextBiggest) {
+    } else if (numArray[i] > nextBiggest && numArray[i] != biggest) {
       nextBiggest = numArray[i];
     }
   }
