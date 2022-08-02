86fd14075e31b8b8e1063184c9a6a9f0aa24be80
Karoline-S <karoline.amy@gmail.com>
2022-08-02 19:55:13 +1000

    test - re-arrange lines

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
 const list = require('./100-data').list;
-
-const newList = list.map((x, index) => x * index);
-
 console.log(list);
-console.log(newList);
+
+const mapList = list.map((x, index) => x * index);
+console.log(mapList);
