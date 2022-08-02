d8aaee117ee49cf62da4900ca8590d298907f1b5
Karoline-S <karoline.amy@gmail.com>
2022-08-02 17:37:57 +1000

    build - add task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let columns = '';
+    for (let i = 0; i < this.width; i++) {
+      columns += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(columns);
+    }
+  }
+
+  rotate () {
+    const hold = this.height;
+    this.height = this.width;
+    this.width = hold;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
