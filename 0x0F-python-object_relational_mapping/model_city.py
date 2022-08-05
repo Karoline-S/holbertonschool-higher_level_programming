ad049d69bada0ed447ba5cdd3e16ce3e8e0fee59
Karoline-S <karoline.amy@gmail.com>
2022-08-04 21:24:43 -0700

    fix - add docstring

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,12 +5,15 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
 
 Base = declarative_base()
 
 
 class City(Base):
+    """defines class City with tablename cities
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id", nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
