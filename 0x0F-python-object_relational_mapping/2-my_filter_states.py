ca8e3257935aeafeea96e5fd516fa20060295a87
Karoline-S <karoline.amy@gmail.com>
2022-08-04 20:48:12 -0700

    fix - case sensitivity

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,18 +6,13 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8")
-
+    conn = MySQLdb.connect(host="localhost", port=3306,\
+                           user=sys.argv[1], passwd=sys.argv[2],\
+                           db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states \
-    WHERE name LIKE '{}' \
-    ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states\
+    WHERE name LIKE BINARY '{}'\
+    ORDER BY id".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
