41911581990d35b22e3d3ac7ad311fe391702669
Karoline-S <karoline.amy@gmail.com>
2022-08-16 21:40:29 -0700

    fix - use correct option

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 # sends a request to passed in URL, displays body of responses with status code 200
 
-status_code=$(curl -sw "%{http_code}" "$1" | sed 's/[^0-9]*//g')
-
-if [ "$status_code" == "200" ]
-then
-    curl "$1"
-fi
+curl -s "$1" -L
