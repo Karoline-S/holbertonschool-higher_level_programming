7661fec9ceb6cd25b1f6716b4cf3b8df3c63aafd
Karoline-S <karoline.amy@gmail.com>
2022-08-19 08:32:54 +1000

    fix - add whitespace

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """ fetch a url with urllib """
 import urllib.request
 
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
