40f660a4a3bb43eaf7de7bc78d29f1cec5e484a6
Karoline-S <karoline.amy@gmail.com>
2022-08-18 11:37:24 +1000

    test - move H flag to end

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes request to a url using PUT method and updating data and header
-curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool"
