4a78fc06c3fd7a2677086b5a65872dbf604668e2
Karoline-S <karoline.amy@gmail.com>
2022-08-18 14:46:59 +1000

    fix - not executed if imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print(f'Body response:\n \
-        \t- type: {html}\n \
-        \t- content: {type(html)}\n \
-        \t- utf8 content: {html.decode()}')
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print(f'Body response:\n \
+            \t- type: {html}\n \
+            \t- content: {type(html)}\n \
+            \t- utf8 content: {html.decode()}')
