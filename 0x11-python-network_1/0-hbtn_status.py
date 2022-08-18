0d1bea6a6e40db8f9013525e7e04d7e4dcf864f4
Karoline-S <karoline.amy@gmail.com>
2022-08-19 09:04:54 +1000

    remove tabulation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:\n \
-            \t- type: {}\n \
-            \t- content: {}\n \
-            \t- utf8 content: {}'.format(type(html), html, html.decode()))
+            - type: {}\n \
+            - content: {}\n \
+            - utf8 content: {}'.format(type(html), html, html.decode()))
