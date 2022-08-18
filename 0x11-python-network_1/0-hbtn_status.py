fcade6d99541781ecd1576bfbc700ce03b8cd8a9
Karoline-S <karoline.amy@gmail.com>
2022-08-19 08:13:19 +1000

    fix - output order

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print(f'Body response:\n \
-            \t- type: {html}\n \
-            \t- content: {type(html)}\n \
+            \t- type: {type(html)}\n \
+            \t- content: {html}\n \
             \t- utf8 content: {html.decode()}')
