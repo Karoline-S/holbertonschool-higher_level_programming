0a3600611dfeef355b796b1fa8660ddf9127c719
Karoline-S <karoline.amy@gmail.com>
2022-08-19 16:03:10 -0700

    test - try except for key

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,4 +6,8 @@ from sys import argv
 
 if __name__ == "__main__":
     r = requests.get(argv[1])
-    print(r.headers['X-Request-Id'])
+    try:
+        value = r.headers['X-Request-ID']
+    except KeyError:
+        value = None
+    print(value)
