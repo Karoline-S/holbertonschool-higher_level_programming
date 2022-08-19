5a823e660a702c92a817f9693978d499ede98323
Karoline-S <karoline.amy@gmail.com>
2022-08-19 14:38:59 -0700

    test - spacing

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,6 +6,7 @@ from sys
 from urllib import request, parse
 
 if __name__ == "__main__":
+
     post_dict = {"email": sys.argv[2]}
     encoded_data = parse.urlencode(post_dict)
     post_data = encoded_data.encode("utf-8")
