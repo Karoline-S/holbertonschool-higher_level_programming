d015beb3128ee226eb30a7a9ded0d671453ef9d9
Karoline-S <karoline.amy@gmail.com>
2022-08-21 05:08:35 -0700

    fix - re-order url repo and owner name

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,9 +13,13 @@ if __name__ == "__main__":
         repo_name = owner_name = ""
 
     queryurl = "https://api.github.com/repos/{}/{}/commits". \
-        format(repo_name, owner_name)
+        format(owner_name, repo_name)
     r = requests.get(queryurl)
     json_data = r.json()
     for commits in json_data[:10]:
-        print("{}: {}". \
-              format(commits['sha'], commits['commit']['author']['name']))
+        try:
+            print("{}: {}". \
+                  format(commits['sha'], commits['commit']['author']['name']))
+        except Keyerror as e:
+            print(e)
+            print("key error occurred")
