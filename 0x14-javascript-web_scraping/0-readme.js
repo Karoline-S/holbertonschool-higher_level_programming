2afc70b5c48a1c0ce60b2f00f47b7f1fed3451c9
Karoline-S <karoline.amy@gmail.com>
2022-09-13 06:46:31 +0000

    fix - indentation

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,8 +3,8 @@
 const fs = require('fs');
 
 try {
-    const contents = fs.readFileSync(process.argv[2], 'utf-8');
-    console.log(contents);
+  const contents = fs.readFileSync(process.argv[2], 'utf-8');
+  console.log(contents);
 } catch (error) {
-    console.error(error);
+  console.error(error);
 }
