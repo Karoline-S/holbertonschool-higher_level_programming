28ad0dd6f60d838f9559cfda8928782fafced49c
Karoline-S <karoline.amy@gmail.com>
2022-09-13 10:05:09 +0000

    fix - remove test print

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,6 @@
 
 const axios = require('axios').default;
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-console.log(url);
 axios.get(url)
   .then(function (response) {
     console.log(response.data.title);
