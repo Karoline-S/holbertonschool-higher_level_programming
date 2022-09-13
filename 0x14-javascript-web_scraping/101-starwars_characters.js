214f38566de720a4ad37d19f8eaa8947c5f548c4
Karoline-S <karoline.amy@gmail.com>
2022-09-13 14:03:37 +0000

    fix - order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,8 +5,9 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 axios.get(url)
   .then(function (response) {
-    for (const item of response.data.characters) {
-      getName(item);
+    const characters = response.data.characters;
+    for (let i = 0; i < characters.length; i++) {
+      getName(characters[i]);
     }
   });
 
