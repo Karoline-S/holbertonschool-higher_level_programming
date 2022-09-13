98ce66070b5a568e177c7c1f0b7f485803ae1678
Karoline-S <karoline.amy@gmail.com>
2022-09-13 12:39:14 +0000

    build - add task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const axios = require('axios').default;
+const url = process.argv[2];
+const dict = {};
+
+axios.get(url)
+  .then(function (response) {
+    const bodyResponse = response.data;
+    for (const todos of bodyResponse) {
+      const id = todos.userId;
+      if (!(id in dict)) {
+        dict[id] = undefined;
+      }
+      if (todos.completed === true) {
+        dict[id] = dict[id] + 1 || 1;
+      }
+    }
+    console.log(dict);
+  });
